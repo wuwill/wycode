87d6859f1285bfdd16853fafd39fa6b55f86c4d2
Wei Yang <wyang@weis-imac.wucon.wustl.edu>
2019-06-26 13:11:11 -0500

    find.file; do.heatmap.combined

--- ngs/chipseq.heatmap.R
+++ ngs/chipseq.heatmap.R
@@ -1,4 +1,5 @@
 library(EnrichedHeatmap)
+# require find.file.R::get.file; atac.dir / chip.dir (group; top.path; dir)
 read.bigwig2rle <- function(bigwig, peaks=NULL, bp=500, aggregate=TRUE, group=gsub(".bigwig", "", bigwig), fun=function(x) x, ...){ #{{{
     if(is.null(peaks)) signal <- import(bigwig, format="BigWig", as="Rle")
     if(!is.null(peaks)){
@@ -124,4 +125,120 @@ my.mk.signal.histgram <- function(aggregated.signal, file="", i.sel=1:ncol(aggre
     if(is.null(legend.items)) legend.items <- 1:n
     legend('topright', legend=legend.items, col=col, lty=1, lwd=3, cex=0.8, border=0)
     matplot(x=-nbp:nbp, aggregated.signal, xlab='', ylab='', type='l', col=col, lty=1, lwd=3, main="")
+} #}}}
+
+# new versions of functions
+get.heat.mat <- function(group, peaks, bp=500, ..., atac=TRUE){ #{{{
+    peaks <- resize(peaks, width=1, fix="center")
+    bigwig <- get.file(group, "bw", ..., atac=atac)
+    signal <- import(bigwig, selection=BigWigSelection(peaks+bp), format="BigWig")
+    mat1 <- normalizeToMatrix(signal, peaks, value_column="score", extend=bp, mean_mode="w0")
+    return(mat1)
+} #}}}
+read.bigwig2rle <- function(group, peaks=NULL, bp=500, aggregate=TRUE, ..., atac=TRUE){ #{{{
+    bigwig <- get.file(group, "bw", atac=atac)
+    signal <- if(is.null(peaks)) signal <- import(bigwig, format="BigWig", as="Rle") else
+        import(bigwig, selection=BigWigSelection(peaks+bp), format="BigWig", as="Rle")
+    if(!is.null(peaks)){
+        peaks <- resize(peaks, width=bp*2+1, fix="center")
+        signal <- signal[peaks]
+    }
+    if(aggregate){ #{{{
+        signal <- sapply(signal, as.vector)
+        return(apply(signal, 1, mean))
+    } #}}}
+    return(signal)
+} #}}}
+my.mk.signal.histgram <- function(aggregated.signal, col=NULL, ...){ #{{{
+    if(is.list(aggregated.signal)) aggregated.signal <- sapply(aggregated.signal, I)
+    nbp <- (nrow(aggregated.signal) - 1)/2
+    n <- ncol(aggregated.signal)
+    if(is.null(col)) col <- cls10[1:n]
+    matplot(x=-nbp:nbp, aggregated.signal, xlab='', ylab='', type='l', col=col, lty=1, lwd=3, main="", ...)
+    legend('topright', legend=colnames(aggregated.signal), col=col, lty=1, lwd=3, cex=0.8, border=0)
+    matplot(x=-nbp:nbp, aggregated.signal, xlab='', ylab='', type='l', col=col, lty=1, lwd=3, main="", ...)
+} #}}}
+do.heatmaps.combined <- function(group.list, col.list, atac.list, peaks, task.name, orderBy=c(1, 1), orderrByDiff=FALSE, heat.mat.list=NULL){ #{{{
+    generate.heat.mats <- function(groups, atac=TRUE){ #{{{
+        heat.mats <- lapply(groups, get.heat.mat, peaks=peaks, bp=5000, atac=atac)
+        names(heat.mats) <- groups
+        return(heat.mats)
+    } #}}}
+    generate.heatmaps <- function(heat.mats, col, row_order){ #{{{
+        title_cex <- gpar(cex=0.6)
+        col_fun = colorRamp2(quantile(unlist(heat.mats), c(0, 0.98)), c("white", col))
+        heatmaps <- lapply(names(heat.mats), function(x)
+                           EnrichedHeatmap(heat.mats[[x]],
+                                           top_annotation=NULL,
+                                           col=col_fun,
+                                           row_order=row_order,
+                                           name="",
+                                           column_title=x,
+                                           show_heatmap_legend=FALSE,
+                                           column_title_gp=title_cex))
+        names(heatmaps) <- names(heat.mats)
+        n <- length(heatmaps)
+        if(n<1) return()
+        for(i in seq_along(heatmaps)){ #{{{
+            p <- if(i==1) heatmaps[[1]] else p + heatmaps[[i]]
+        } #}}}
+        return(p)
+    } #}}}
+    if(is.null(heat.mat.list))
+        heat.mat.list <- lapply(seq_along(group.list), function(i) generate.heat.mats(group.list[[i]], atac.list[[i]]))
+    row_order <- if(orderrByDiff) order(peaks$Fold) else {
+        ave1 <- apply(heat.mat.list[[orderBy[1]]][[orderBy[2]]][,41:60], 1, mean)
+        order(-ave1)
+    }
+    for(i in seq_along(group.list)){ #{{{
+        ploti <- generate.heatmaps(heat.mat.list[[i]], col.list[[i]], row_order)
+        p <- if(i==1) ploti else p + ploti
+    } #}}}
+    n <- length(unlist(group.list))
+    if(n<1) return()
+    pdf(paste0(task.name, "_heatmap", ".pdf"), width=1.2*n, height=8); on.exit(dev.off())
+    print(p)
+    return(invisible(heat.mat.list))
+} #}}}
+do.heatmaps.temp.peaks <- function(groups, peaks, task.name, atac=TRUE){ #{{{
+        i.sel <- 1
+        heat.mats <- lapply(groups, get.heat.mat, peaks=peaks, bp=5000, atac=atac)
+        mat.names <- names(heat.mats) <- groups
+        row_order1 <- order(peaks$Fold)
+        ave1 <- apply(heat.mats[[i.sel]][,41:60], 1, mean)
+        row_order2 <- order(-ave1)
+        col_fun = colorRamp2(quantile(unlist(heat.mats), c(0, 0.98)), c("white", "red"))
+        #col_fun_k4 = colorRamp2(quantile(unlist(heat.mats[ik4]), c(0, 0.99)), c("white", "darkgreen"))
+        #col_fun_k27 = colorRamp2(quantile(unlist(heat.mats[ik27]), c(0, 0.99)), c("white", "blue"))
+        #get.col_fun <- function(x) if(grepl("k4$", x)) col_fun_k4 else if(grepl("k27$", x)) col_fun_k27 else col_fun
+        get.col_fun <- function(x) col_fun
+        title_cex <- gpar(cex=0.6)
+
+        my.mk.heatmaps <- function(row_order, task.name, name){ #{{{
+            heatmap1 <- EnrichedHeatmap(heat.mats[[i.sel]],
+                                        top_annotation=NULL,
+                                        col=col_fun,
+                                        row_order=row_order,
+                                        name="",
+                                        column_title=names(heat.mats)[i.sel],
+                                        column_title_gp=title_cex)
+            heatmaps <- c(heatmap1, lapply(names(heat.mats)[-i.sel], function(x)
+                                           EnrichedHeatmap(heat.mats[[x]],
+                                                           top_annotation=NULL,
+                                                           col=get.col_fun(x),
+                                                           row_order=row_order,
+                                                           column_title=x,
+                                                           show_heatmap_legend=FALSE,
+                                                           column_title_gp=title_cex)))[order(c(i.sel, seq_along(heat.mats)[-i.sel]))]
+            names(heatmaps) <- names(heat.mats)
+            n <- length(heatmaps)
+            if(n<1) return()
+            for(i in seq_along(heatmaps)){ #{{{
+                p <- if(i==1) heatmaps[[1]] else p + heatmaps[[i]]
+            } #}}}
+            pdf(paste0(task.name, "_heatmap_", name,  ".pdf"), width=1.5*n, height=8); on.exit(dev.off())
+            print(p)
+        } #}}}
+        my.mk.heatmaps(row_order1, task.name, "orderBy_DBAFoldChange")
+        my.mk.heatmaps(row_order2, task.name, "orderBy_FirstColumn")
 } #}}}
--- ngs/find.file.R
+++ ngs/find.file.R
@@ -4,18 +4,84 @@ find.file <- function(path, pattern, pattern2=NULL, vpattern=NULL){ #{{{
     if(!is.null(vpattern)) cmd <- paste0(cmd, " | grep -v '", vpattern, "'")
     return(system(cmd, TRUE))
 } #}}}
-get.chip.file <- function(path, type, pooled=TRUE){ #{{{
+get.new.chip <- function(path, type, pooled=TRUE){ #{{{
     # type in bam, bw, peak
     ret <-
         if(pooled){ #{{{
             if(type %in% "peak") find.file(path, "optimal_peak.narrowPeak.gz") else
-                if(type %in% "bw") find.file(path, "*pooled.fc.signal.bigwig") else
+                if(type %in% "bw") find.file(path, "*fc.signal.bigwig", pattern2="pooled") else 
                     ""
         } else { # for replicates
             if(type %in% "peak") find.file(path, "*bfilt.narrowPeak.gz", pattern2="call-macs2/") else
-                if(type %in% "bam") find.file(path, "*nodup.bam", pattern2="call-filter/", vpattern="inputs") else
+                if(type %in% "bam") find.file(path, "*nodup.bam", pattern2="call-filter/", vpattern="inputs") else 
                     if(type %in% "bw") find.file(path, "*fc.signal.bigwig", pattern2="call-macs2/", vpattern="inputs") else
                         ""
         } #}}}
     return(ret)
+} #}}}
+get.new.atac <- function(path, type, pooled=TRUE){ #{{{
+    # type in bam, bw, peak
+    ret <-
+        if(pooled){ #{{{
+            if(type %in% "peak") find.file(path, "optimal_peak.narrowPeak.gz") else
+                if(type %in% "bw") find.file(path, "*pooled.fc.signal.bigwig") else 
+                    ""
+        } else { # for replicates
+            if(type %in% "peak") find.file(path, "*bfilt.narrowPeak.gz", pattern2="call-macs2/") else
+                if(type %in% "bam") find.file(path, "*nodup.bam", pattern2="call-filter/", vpattern="inputs") else 
+                    if(type %in% "bw") find.file(path, "*fc.signal.bigwig", pattern2="call-macs2/", vpattern="inputs") else
+                        ""
+        } #}}}
+    return(ret)
+} #}}}
+get.prev.chip <- function(path, type, pooled=TRUE){ #{{{
+    # type in bam, bw, peak
+    ret <-
+        if(pooled){ #{{{
+            if(type %in% "peak") find.file(path, "*filt.narrowPeak.gz", pattern2="optimal_set") else
+                if(type %in% "bw") {
+                    f <- find.file(path, "*fc.signal.bw")
+                    if(length(f)>1) f <- grep("pooled_rep", f, value=TRUE)
+                    f
+                } else 
+                    ""
+        } else { # for replicates
+            if(type %in% "peak") find.file(path, "*filt.narrowPeak.gz", pattern2="macs2/rep") else
+                if(type %in% "bam") find.file(path, "*.markdup.bam") else  #@ this does not work for now
+                    if(type %in% "bw") find.file(path, "*fc.signal.bw", pattern2="macs2/rep", vpattern="inputs") else
+                        ""
+        } #}}}
+    return(ret)
+} #}}}
+get.prev.atac <- function(path, type, pooled=TRUE){ #{{{
+    # type in bam, bw, peak
+    ret <-
+        if(pooled){ #{{{
+            if(type %in% "peak") find.file(path, "*narrowPeak.gz", pattern2="optimal") else
+                if(type %in% "bw") find.file(path, "*fc.signal.bigwig", pattern2="pooled_") else 
+                    ""
+        } else { # for replicates
+            if(type %in% "peak") find.file(path, "*filt.narrowPeak.gz", pattern2="macs2/rep") else
+                if(type %in% "bam") find.file(path, "*nodup.bam", pattern2="align/rep") else 
+                    if(type %in% "bw") find.file(path, "*fc.signal.bigwig", pattern2="macs2/rep") else
+                        ""
+        } #}}}
+    return(ret)
+} #}}}
+get.chip.file <- function(group, type, ..., pattern=NULL){ #{{{
+    i <- match(group, chip.dir$group)
+    path <- file.path(chip.dir$top.path[i], chip.dir$dir[i])
+    get.file <- if(chip.dir$top.path[i] %in% new.pipe.list) get.new.chip else get.prev.chip
+    ret <- get.file(path, type, ...)
+    if(!is.null(pattern)) grep(pattern, ret, value=TRUE) else return(ret)
+} #}}}
+get.atac.file <- function(group, type, ..., pattern=NULL){ #{{{
+    i <- match(group, atac.dir$group)
+    path <- file.path(atac.dir$top.path[i], atac.dir$dir[i])
+    get.file <- if(atac.dir$top.path[i] %in% new.pipe.list) get.new.atac else get.prev.atac
+    ret <- get.file(path, type, ...)
+    if(!is.null(pattern)) grep(pattern, ret, value=TRUE) else return(ret)
+} #}}}
+get.file <- function(..., atac=TRUE){ #{{{
+    if(atac) get.atac.file(...) else get.chip.file(...)
 } #}}}
